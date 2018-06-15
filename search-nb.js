0df0f207f6e0147c9cdf2e57a98b6376552f025c
oyvind Gjesdal <oyvind@localhost.localdomain>
2018-06-16 00:39:47 +0200

    query param as string

--- search-nb.js
+++ search-nb.js
@@ -5,7 +5,7 @@ console.log(query);
 SaxonJS.transform({
          stylesheetLocation: "xsl/search-nb.sef",
          initialTemplate: "initialTemplate",
-         styleSheetParams: { "q" : query}}
+         styleSheetParams: { "Q{}q" : query}}
 )
 console.log(query);
 }
