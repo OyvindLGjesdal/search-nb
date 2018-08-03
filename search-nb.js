6b065b44a4dcfe3f0e98139efb7bbda3d007fdc9
oyvind Gjesdal <oyvind@localhost.localdomain>
2018-08-03 22:38:18 +0200

    updated js

--- search-nb.js
+++ search-nb.js
@@ -2,13 +2,9 @@
 
 var basicResult = new Object();
 
-function transformSaxon(query){
-console.log(query);
-
 SaxonJS.transform({
          stylesheetLocation: "xsl/search-nb.sef",
-         initialTemplate: "initialTemplate",
-         stylesheetParams: { "q" : query}}
+         initialTemplate: "initialTemplate"}
 )
 console.log(query);
-}
+
